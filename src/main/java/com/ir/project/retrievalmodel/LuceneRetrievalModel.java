a2896950360dcfc2b43cbf1dafff3c3a3623c5bb
karantyagi <tyagi.k@husky.neu.edu>
2018-04-22 12:56:05 -0400

    Adding LuceneIdexer

--- src/main/java/com/ir/project/retrievalmodel/LuceneRetrievalModel.java
+++ src/main/java/com/ir/project/retrievalmodel/LuceneRetrievalModel.java
@@ -1,4 +1,26 @@
 package com.ir.project.retrievalmodel;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+
 public class LuceneRetrievalModel {
+
+    private String query;
+    private String indexDirPath;
+
+
 }
